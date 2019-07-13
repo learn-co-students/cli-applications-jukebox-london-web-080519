9f05e1cca3f31dd617fbc7bb4fb703112a758317
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 19:56:13 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,29 +1,16 @@
 # Add your code here
 
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
-
 def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
+  puts  "I accept the following commands:"
+  puts  "- help : displays this help message"
+  puts  "- list : displays a list of songs you can play"
+  puts  "- play : lets you choose a song to play"
+  puts  "- exit : exits this program"
 end
 
 def list(songs)
-  i = 0
-  songs.each do |song|
-    puts "#{i}. #{song}"
-    i++
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
   end
 end
 
@@ -31,37 +18,36 @@ def play(songs)
   puts "Please enter a song name or number:"
   response = gets.chomp
 
-  solution = songs.detect do |song|
-    response == song
+  if response.to_i >= 1 && response.to_i <= songs.length
+    puts "Playing #{songs[response.to_i-1]}"
+  elsif songs.include?(response)
+    puts "Playing #{songs.find{|song| song == response}}"
+  else
+    puts "Invalid input, please try again"
   end
-
-  if !solution
-    songs.length > response ? solution = "Invalid input, please try again" : solution = response - 1
-  end
-
-  puts solution
 end
 
 def exit_jukebox
   puts "Goodbye"
 end
 
-def run
+def run(songs)
   help
-  puts "Please enter a command:"
-  input = gets.strip
-  case input
-  when "exit"
-    exit_jukebox
-    break
-  when "list"
-    list(songs)
-  when "play"
-    play(songs)
-  when "help"
-    help
-  else
-    puts "Invalid input, please try again"
-    help
-  end    
+  while true do
+    puts "Please enter a command:"
+    input = gets.strip
+    case input
+    when "exit"
+      exit_jukebox
+      break
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "help"
+      help
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
