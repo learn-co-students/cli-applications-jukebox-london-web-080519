f3c44158e15fd7009311f27c6d14acdaceb14576
Angelina Shin Manley <ashin05@gmail.com>
2019-07-29 13:12:09 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,72 @@
-# Add your code here
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
+
+#puts "Enter your name:"
+#users_name = gets.chomp
+
+#puts say_hello(users_name)
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+
+  songs.each_with_index do | song, index |
+    if song_choice.to_i == index + 1 || song_choice == song
+      puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  command = gets.chomp
+  if command === "exit"
+    exit_jukebox
+  elsif command === "list"
+    list(songs)
+    run(songs)
+  elsif command === "play"
+    play(songs)
+    run(songs)
+  elsif command === "help"
+    help
+    run(songs)
+  else
+    puts "Invalid input, please try again"
+    run(songs)
+  end
+end
